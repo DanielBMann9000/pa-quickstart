7fa6a28c7e577a098473065949346f9f0e661fbe
Daniel Mann <dbmann@gmail.com>
2015-04-01 13:55:43 -0400

    added basic PA Client factory implementation

--- Client/PASample/PASample.Wpf/PAClientFactory.cs
+++ Client/PASample/PASample.Wpf/PAClientFactory.cs
@@ -1,6 +1,41 @@
-﻿namespace PASample.Wpf
+﻿using PreEmptive.Analytics.Common;
+using PreEmptive.Analytics.NET;
+
+namespace PASample.Wpf
 {
     internal class PAClientFactory
     {
+        private static PAClient client;
+
+        public static PAClient GetPAClient(string instance = null, string username = null)
+        {
+            if (client != null)
+            {
+                return client;
+            }
+            if (instance == null)
+            {
+                instance = App.GetLicenseKey();
+            }
+
+            var configuration = new Configuration("7d2b02e0-064d-49a0-bc1b-4be4381c62d3", "42AC2020-ABA1-9069-A2BD-98072B33309A")
+            {
+                ApplicationName = "Quickstart Sample",
+                ApplicationType = "Sample",
+                CompanyName = "My Company",
+                InstanceID = instance,
+                ApplicationVersion = "1.0",
+                Endpoint = "so-s.info/endpoint",
+                UseSSL = false,
+                FullData = true
+            };
+
+            configuration.StopBehavior.SessionExtensionWindow = 15000;
+            configuration.SupportOfflineStorage = true;
+
+            client = new PAClient(configuration);
+
+            return client;
+        }
     }
 }
